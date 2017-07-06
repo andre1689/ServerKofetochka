c6ad487a253f725becb6962d484d17c9a235b1d1
Andrew Smola <andrew.smol.g@gmail.com>
2017-07-06 12:55:26 +0000

    Create get_array_name_drink

---
+++ get_array_name_drink.php
@@ -0,0 +1,21 @@
+<?php
+require_once __DIR__ . '/db_config.php';
+$con = mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD, DB_DATABASE);
+if (mysqli_connect_errno()) {
+    printf("Не удалось подключиться: %s\n", mysqli_connect_error());
+    exit();
+}
+$Table = $_REQUEST[Table];
+$r = mysqli_query($con, "SELECT Name_Drink FROM `".$Table."` GROUP BY Name_Drink");
+if (mysqli_num_rows($r) > 0)
+{
+	$response["names_drink"] = array();
+        while($row=mysqli_fetch_array($r))
+        {
+                $name_drink = array();
+		$name_drink[Name_Drink] = $row[Name_Drink];
+		array_push($response["names_drink"], $name_drink);
+	}
+}
+echo (json_encode($response));
+?>
